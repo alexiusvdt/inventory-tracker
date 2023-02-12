04eab53a3d396fd5fcd10ae5414e637d4c360e81
Alex Johnson <alex.johnson293@gmail.com>
2023-02-12 15:10:36 -0800

    grammar pass, begin styling

--- bean-tracker/src/components/Header.js
+++ bean-tracker/src/components/Header.js
@@ -3,8 +3,8 @@ import React from "react";
 function Header(){
   return (
     <React.Fragment>
-    <h1>BeanBois & Co.</h1>
-    <p>The freshest burlap bean bags in Boston!</p>
+    <h1>BeanBoys & Bros</h1>
+    <p>The freshest burlap bean bags in Boston! Brew brilliantly with the best beans around! </p>
     </React.Fragment>
   );
 }
